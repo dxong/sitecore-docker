092493459a5185c2a78e0cdde732d6fd7d0f02ef
Rick van Dam <rick-van-dam@outlook.com>
2019-05-07 16:50:40 +0200

    WIP

--- Generate-Self-Signed-Certificate.ps1
+++ Generate-Self-Signed-Certificate.ps1
@@ -1,12 +1,12 @@
 Param(
     $dnsName = "",
     $file = "",
-    $secret = "secret"
+    $secret = "secret",
+    $signer
 )
 
-[X509Certificate]$cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $dnsName -KeyExportPolicy Exportable `
--Provider "Microsoft Strong Cryptographic Provider" `
--HashAlgorithm "SHA256";
+$cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $dnsName -Signer $signer -KeyExportPolicy Exportable -Provider "Microsoft Strong Cryptographic Provider" `
+-HashAlgorithm "SHA256"
 
 $pwd = ConvertTo-SecureString -String $secret -Force -AsPlainText
 
